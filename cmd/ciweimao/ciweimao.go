fe310bef730a2e414124856579b96671713a54f7
frely <frely161@gmail.com>
2024-07-05 14:12:28 +0800

    ciweimao: Update get book info

--- cmd/ciweimao/ciweimao.go
+++ cmd/ciweimao/ciweimao.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"runtime"
+	"time"
 
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
@@ -12,13 +14,16 @@ import (
 const indexUrl string = "https://www.ciweimao.com/book/100171528"
 
 var (
-	browser  string
-	bookName string
-	author   string
+	browser     string
+	bookName    string
+	author      string
+	chapterList [][]string
 )
 
 func main() {
 	sysEnv()
+	getIndex()
+	fmt.Println(chapterList)
 
 }
 
@@ -35,9 +40,14 @@ func sysEnv() {
 func getIndex() {
 	u := launcher.New().Bin(browser).MustLaunch()
 	page := rod.New().ControlURL(u).MustConnect().MustPage(indexUrl)
+	defer page.MustClose()
+	page.Timeout(5 * time.Second)
 
-	// 查找书籍信息
-	metaTags, _ := page.Elements("meta")
+	// 查找书籍信息：书名，作者
+	metaTags, err := page.Elements("meta")
+	if err != nil {
+		log.Fatalln("no find meta")
+	}
 	for _, meta := range metaTags {
 		property := meta.MustAttribute("property")
 		content := meta.MustAttribute("content")
@@ -47,5 +57,29 @@ func getIndex() {
 		if property != nil && *property == "og:novel:author" {
 			author = *content
 		}
+	}
+	chapterPage, err := page.Element(".btn-more")
+	if err != nil {
+		log.Fatalln("not find chapterPage")
+	}
+	chapterPageUrl := chapterPage.MustAttribute("href")
+
+	// 查找章节列表
+	index := rod.New().ControlURL(u).MustConnect().MustPage(*chapterPageUrl)
+	defer index.MustClose()
+	index.Timeout(5 * time.Second)
+	bookChapterBox, _ := index.MustElement(".book-chapter").Elements("a")
+	for _, v := range bookChapterBox {
+		chapterName, err := v.Attribute("href")
+		if err != nil {
+			continue
+		}
+		chapterUrl, err := v.Text()
+		if err != nil || chapterUrl == "" {
+			continue
+		}
+		tmpList := make([]string, 0)
+		tmpList = append(tmpList, *chapterName, chapterUrl)
+		chapterList = append(chapterList, tmpList)
 	}
 }
